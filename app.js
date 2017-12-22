58ad75e0c6b5982b521ea16674d798338a61c4c8
Emerael <emerael@yandex.ru>
2017-12-22 17:22:44 +0300

    Третье задание тип

---
+++ app.js
@@ -0,0 +1,5 @@
+﻿$('#root').html("<ul><li><span>Сделать задание #3 по web-программированию</span><button onclick='$(this).parent().remove()'>Удалить</button></li></ul>");
+function add_task(){
+	$('#root ul').append('<li><span>' + $('#add_task_input').val() + '</span><button onclick="$(this).parent().remove()">Удалить</button></li></ul>');
+}
+$('#root').append("<input id=add_task_input type='text'><button id=add_task onclick=add_task()>Добавить задание</button>")
