5058db83e510665cbf7347ad67b114356aea6ea4
Embery <emerael@ya.ru>
2020-07-27 07:20:18 +0300

    закинула в стор товар с id, включающим уже существующие
    just 4 test idFilter(не будет ли он выдавать и полностью входящие идентификатор). Было очевидно, что не будет, но пусть будет для полноты покрытия

--- js/app/store/products.js
+++ js/app/store/products.js
@@ -7,6 +7,7 @@ Ext.define('test.store.products', {
     data: [
         {id:'1',name:"Товар 1",descr:"Ололо",price:"100.5",amount:'5'},
         {id:'2',name:"Товарищ 2",descr:"Я водитель нло",price:"200.5",amount:'3'},
-        {id:'3',name:"Товар 3",descr:"Жесть какая-то",price:"500",amount:'0'}
+        {id:'3',name:"Товар 3",descr:"Жесть какая-то",price:"500",amount:'0'},
+        {id:'23',name:"Чотовское необычное",descr:"Необычная дичь дорогая беполезная",price:"500",amount:'0'}
     ]
 });
